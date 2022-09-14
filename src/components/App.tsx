3e5eeda70bb2e341409dc0b8b64214dcfb716367
Miranda Guimarães <armg@gft.com>
2022-09-14 15:22:57 -0300

    [Bug] Fixing router.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { BrowserRouter as HashRouter, Switch, Route } from "react-router-dom";
-
 import Nodes from "../containers/Nodes";
-import NotFoundPage from "./NotFoundPage";
 
 const App: React.FC = () => {
-  return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Nodes} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </HashRouter>
-  );
+  return <Nodes />;
 };
 
 export default App;
